ce0f89732b18880369ee478b4a78a2a6fcd0edf5
R Gray <rgray@rs-macbook-pro.home>
2020-03-29 19:42:38 +0100

    Updates

---
+++ precache-manifest.270816fc3814e9292043e28b9af7ac08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b5444b97655ece9787bf3d48854a67",
+    "url": "/monters-rolodex/index.html"
+  },
+  {
+    "revision": "b7a6c7d8a672e7c0c6b7",
+    "url": "/monters-rolodex/static/css/main.7092eed3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a6c7d8a672e7c0c6b7",
+    "url": "/monters-rolodex/static/js/main.ccdcf3f0.chunk.js"
+  },
+  {
+    "revision": "d675114f4bc5dcac6e2b",
+    "url": "/monters-rolodex/static/js/runtime-main.e042b115.js"
+  }
+]);
